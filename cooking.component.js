84aaf66009baf546eb1b9ce3ee1ede9b084683b6
Pekhenka1 <137805390+Pekhenka1@users.noreply.github.com>
2025-09-30 11:40:52 +0300

    Update cooking.component.js

--- cooking.component.js
+++ cooking.component.js
@@ -1,6 +1,6 @@
 var cooking = require('cooking');
 var Components = require('../components.json');
-var webpack = require('webpack');
+var webpack = require('webpack');dwq
 var config = require('./config');
 
 cooking.set({
